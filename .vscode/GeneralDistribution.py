d11ad9401c18bcd72fa286f83639ddf046a74048
Ricardo Lino <ricardo@costalino.com>
2019-05-16 13:09:15 -0300

    Ajuste da GeneralDistribution

--- .vscode/GeneralDistribution.py
+++ .vscode/GeneralDistribution.py
@@ -15,3 +15,26 @@ class Distribution:
         self.stdev = sigma
         self.data = []
 
+
+    def read_data_file(self, file_name):
+    
+        """Function to read in data from a txt file. The txt file should have
+        one number (float) per line. The numbers are stored in the data attribute.
+                
+        Args:
+            file_name (string): name of a file to read from
+        
+        Returns:
+            None
+        
+        """
+            
+        with open(file_name) as file:
+            data_list = []
+            line = file.readline()
+            while line:
+                data_list.append(int(line))
+                line = file.readline()
+        file.close()
+    
+        self.data = data_list
