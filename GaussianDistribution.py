3942e20f6c93abe53a1bfaded5223e1e494ad96c
Ricardo Lino <ricardo@costalino.com>
2019-05-16 13:22:22 -0300

    Ajuste Diretorios

--- GaussianDistribution.py
+++ GaussianDistribution.py
@@ -1,30 +1,6 @@
 import math
 import matplotlib.pyplot as plt
 from GeneralDistribution import Distribution
-
-    def read_data_file(self, file_name):
-    
-        """Function to read in data from a txt file. The txt file should have
-        one number (float) per line. The numbers are stored in the data attribute.
-                
-        Args:
-            file_name (string): name of a file to read from
-        
-        Returns:
-            None
-        
-        """
-            
-        with open(file_name) as file:
-            data_list = []
-            line = file.readline()
-            while line:
-                data_list.append(int(line))
-                line = file.readline()
-        file.close()
-    
-        self.data = data_list
-
         
 class Gaussian():
     """ Gaussian distribution class for calculating and 
